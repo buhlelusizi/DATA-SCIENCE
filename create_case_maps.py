3651a5ffe5ca0136116ee7776d3685582a43cde1
Buhle Lusizi <70804695+buhlelusizi@users.noreply.github.com>
2021-07-27 22:40:12 +0200

    Add files via upload

---
+++ create_case_maps.py
@@ -0,0 +1,70 @@
+
+import pandas as pd
+import plotly.express as px
+from datetime import datetime, timedelta
+
+
+from helper import load_relevant_data, us_state_abbrev
+
+def plot_usa_case_map(filename=None, day=None):
+	df = load_relevant_data()
+	dates = list(df.columns)
+	df = df.groupby('Province_State')[dates].agg('sum')
+	create_usa_figure(df, filename, day)
+
+def plot_global_case_map(filename=None, day=None):
+	df = load_relevant_data(us_data=False)
+	dates = list(df.columns)
+	df = df.groupby('Country/Region')[dates].agg('sum')
+	create_global_figure(df, filename, day)
+
+def create_usa_figure(df, filename, day):
+	day = day if day else yesterday 
+	df['Cases'] = df.diff(axis=1)[day]
+	df['state'] = [us_state_abbrev.get(x, None) for x in list(df.index)]
+	
+
+	fig = px.choropleth(df,
+                    locations="state",
+                    locationmode="USA-states",
+                    scope="usa",
+                    color="Cases",
+                    hover_name="state",
+                    color_continuous_scale='Peach',
+                    title=f"US Daily Cases, {day}",
+                    width=1000,
+                    range_color=[0,3000])
+
+	fig.update_layout(margin=dict(l=0, r=0, t=70, b=0), title={"font": {"size": 20}, "x":0.5},)
+	filename = filename if filename else "usa_chart.png"
+	fig.write_image(filename, engine='kaleido')
+
+def create_global_figure(df, filename, day):
+
+	df['Cases'] = df.diff(axis=1)[day]
+	df['Country'] = df.index
+
+	fig = px.choropleth(df,
+                    locations="Country",
+                    locationmode="country names",
+                    scope="world", 
+                    color="Cases",
+                    hover_name="Country",
+                  
+                    color_continuous_scale='Peach',
+                    title=f"Global Daily Cases, {day}",
+                    width=1000,
+                    
+                    range_color=[0,50000])
+
+	fig.update_layout(margin=dict(l=0, r=0, t=70, b=20), title={"font": {"size": 20}, "x":0.5},)
+	filename = filename if filename else "global_chart.png"
+	fig.write_image(filename, engine='kaleido')
+
+if __name__ == '__main__':
+	yesterday = (datetime.today() - timedelta(days=1)).strftime("%m/%d/%y")
+	
+	yesterday = "10/10/20"
+
+	plot_usa_case_map(day=yesterday) 
+	plot_global_case_map(day=yesterday) 
